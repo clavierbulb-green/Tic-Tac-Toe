4aecc11881bd6618ee125aad00b455e09de1c3b9
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-12-22 14:18:25 -0800

    Add methods to gameBoard that check for winning combinations

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,4 +1,6 @@
 const gameBoard = (() => {
+  const NUM_ROWS = 3;
+  const NUM_COLUMNS = 3;
   const board = [[],[],[]];
   const mark = (row, column, marker) => {
     if (!board[row][column]) {
@@ -13,11 +15,36 @@ const gameBoard = (() => {
   const isFull = () => {
     return board.flat().length === 9;
   }
+  const rowWin = () => {
+    for (let row = 0; row < NUM_ROWS; row++) {
+      if (board[row].every(mark => mark && mark === board[row][0])) {
+        return true;
+      }
+    }
+  }
+  const colWin = () => {
+    for (let col = 0; col < NUM_COLUMNS; col++) {
+      let column = [board[0][col], board[1][col], board[2][col]];
+      if (column.every(mark => mark && mark === mark[0])) {
+        return true;
+      }
+    }
+  }
+  const diagonalWin = () => {
+    let diag1 = [board[0][0], board[1][1], board[2][2]];
+    let diag2 = [board[0][2], board[1][1], board[2][0]];
+    return diag1.every(mark => mark && mark === diag1[0]) ||
+      diag2.every(mark => mark && mark === diag2[0]);
+  }
+  const isGameOver = () => {
+    return rowWin() || colWin() || diagonalWin();
+  }
   return {
     board,
     mark,
     clear,
     isFull,
+    isGameOver,
   }
 })();
 module.exports = gameBoard;
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -35,3 +35,65 @@ test('a board is full when all nine squares are marked', () => {
   gameBoard.mark(2, 2, 'X');
   expect(gameBoard.isFull()).toBeTruthy();
 });
+
+/* test row wins */
+test('3 squares in a row with the same mark is a win(1)', () => {
+  gameBoard.mark(0, 0, 'X');
+  gameBoard.mark(0, 1, 'X');
+  gameBoard.mark(0, 2, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+test('3 squares in a row with the same mark is a win(2)', () => {
+  gameBoard.mark(1, 0, 'X');
+  gameBoard.mark(1, 1, 'X');
+  gameBoard.mark(1, 2, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+test('3 squares in a row with the same mark is a win(3)', () => {
+  gameBoard.mark(2, 0, 'X');
+  gameBoard.mark(2, 1, 'X');
+  gameBoard.mark(2, 2, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+
+/* test column wins */
+test('3 squares in a column with the same mark is a win(1)', () => {
+  gameBoard.mark(0, 0, 'X');
+  gameBoard.mark(1, 0, 'X');
+  gameBoard.mark(2, 0, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+test('3 squares in a column with the same mark is a win(2)', () => {
+  gameBoard.mark(0, 1, 'X');
+  gameBoard.mark(1, 1, 'X');
+  gameBoard.mark(2, 1, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+test('3 squares in a column with the same mark is a win(3)', () => {
+  gameBoard.mark(0, 2, 'X');
+  gameBoard.mark(1, 2, 'X');
+  gameBoard.mark(2, 2, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+
+/* test diagonal wins */
+test('3 squares in a diagonal with the same mark is a win(1)', () => {
+  gameBoard.mark(0, 0, 'X');
+  gameBoard.mark(1, 1, 'X');
+  gameBoard.mark(2, 2, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+test('3 squares in a diagonal with the same mark is a win(2)', () => {
+  gameBoard.mark(0, 2, 'X');
+  gameBoard.mark(1, 1, 'X');
+  gameBoard.mark(2, 0, 'X');
+  expect(gameBoard.isGameOver()).toBeTruthy();
+});
+
+test('a non-winning combination is not a win', () => {
+  gameBoard.mark(0, 1, 'X');
+  gameBoard.mark(1, 1, 'X');
+  gameBoard.mark(2, 2, 'X');
+  expect(gameBoard.isGameOver()).toBeFalsy();
+});
+
